01493d711774cff0fcea7e436c889c27911737c5
Berry  Semexan <bsemexan@gmail.com>
2018-02-19 06:53:22 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.end_with?("!") || self.include?("?") || self.include?(".") ? self.split.count : 0
+    self.include?("!") || self.include?("?") || self.include?(".") ? self.split.count : 0
   end
 end
