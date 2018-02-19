64182df3c5c6ed460c5fbe92c0294e20f94ee845
Berry  Semexan <bsemexan@gmail.com>
2018-02-19 06:57:55 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,6 +15,6 @@ class String
   end
 
   def count_sentences
-    self.end_with?("!") || self.end_with?("?") || self.end_with?(".") ? self.split : 0
+    self.end_with?("!") || self.end_with?("?") || self.end_with?(".") ? self.count : 0
   end
 end
